7999fc7d98ec3653883da5b191cac2250a2e99b5
George Frey <geofrey19@gmail.com>
2021-06-07 14:53:18 -0300

    Criando DAO

--- pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
+++ pos-java-maven-hibernate/src/test/java/posjavamavenhibernate/TesteHibernate.java
@@ -2,10 +2,26 @@ package posjavamavenhibernate;
 
 import org.junit.Test;
 
+import dao.DaoGeneric;
+import model.UsuarioPessoa;
+
 public class TesteHibernate {
 
 	@Test
 	public void TesteHibernateUtil() {
-		HibernateUtil.getEntityManager();
+		/*HibernateUtil.getEntityManager();*/
+		DaoGeneric<UsuarioPessoa> daoGeneric = new DaoGeneric<UsuarioPessoa>();
+		
+		UsuarioPessoa pessoa = new UsuarioPessoa();
+		
+		pessoa.setIdade(45);
+		pessoa.setLogin("teste");
+		pessoa.setNome("Alex Fernando 2");
+		pessoa.setSenha("123");
+		pessoa.setSobrenome("Egídio");
+		pessoa.setEmail("alex@gmail.com");
+		
+		daoGeneric.salvar(pessoa);
+		
 	}
 }
